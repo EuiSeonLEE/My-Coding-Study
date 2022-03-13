5382f4a8a86aa797a87ffaea208fb58deb1110d8
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-03-13 22:53:47 +0900

    문자열(백준)

--- C++/문자열 반복.cpp
+++ C++/문자열 반복.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main(void){
     string S = "";
-    
+    int R, T = 0;
+
+    while(T < 3){
+    cin >> R;
     cin >> S;
-    S.resize(100);   //100byte로 문자길이 맞추기
+    S.resize(20);   //100byte로 문자길이 맞추기
     S.shrink_to_fit(); //100byte에 맞는 메모리 자동 할당
-    cout << S << endl;
+        
     for(char c = 97; c < 123;c++){
         for(int a = 0;a < S.size();a++){
             if(S.operator[](a) == c){ //operator의 리턴값은 char형
