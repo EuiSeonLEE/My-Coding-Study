edca033149a8e56b011332b6ab07a687a5fefbb8
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-03-26 05:02:48 +0900

    자료형(숫자형, 문자열, 리스트)

--- Python/문법/basic.py
+++ Python/문법/basic.py
@@ -30,4 +30,50 @@ print(e[::-2])#부호가 들어가면 반대로 읽는거임
 h = "hey, hey you, {name} you".format(name = 'yeah')
 print(h)
 
-#말도 안돼...이게 바로...신세계인가...
+h = """hey,
+hey you, 
+{name} you""".format(name = 'yeah')
+print(h)
+
+h = "%0.4f"%3.1421354
+print(h)
+
+h = "hobbbby"
+print(h.count('b'))
+print(h.find('b'))
+
+h = ",".join("abcd")
+print(h)
+h = ",".join(["a","b","c","b","e","f"])
+print(h)
+h = "hi"
+print(h.upper())
+h = "    hi my name is SEX     "
+print(h.strip())
+print(h.split())
+print(h.replace("my","your"))
+
+#리스트
+a = "ABC"
+b = "DEF"
+c = [1,2,"int",["APPLE", "BANANA"]]
+print(c[3])
+print(c[0:2])
+print(c)
+del c[0] #리스트 요소삭세
+print(c)
+c.append("sex") #리스트 요소 추가
+print(c)
+d = [1,6,9,2,10,6,3,5,6,7]
+d.sort() #숫자는 크기순, 알파벳은 a부터 z, 한글은 ㄱ부터 ㅎ
+print(d)
+d.reverse() #순서 뒤집기
+print(d)
+d.insert(0, 11) #리스트 0번째에 숫자 11추가
+print(d)
+d.remove(6) #리스트의 숫자6을 삭제(여러개면 가장 앞에 번째 1개 지움)
+print(d)
+print(d.pop()) #스택의 pop이라고 생각하면 됨(꺼낸 요소 보여줌)
+print(d.count(6)) #리스트에 숫자 6이 몇개있는지 세준다
+d.extend([12,13]) #리스트에 리스트를 뒤로 추가
+print(d)
