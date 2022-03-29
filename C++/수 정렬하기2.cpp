5716acea65a90f0b9ddbc8f741f0a0d4593154cc
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-03-29 10:11:23 +0900

    정렬(백준)

---
+++ C++/수 정렬하기2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    vector<short> arr, arr2;
+    int Num = 0;
+    int arrNum = 0;
+    cin >> Num;
+    
+    for(int a = 0; a < Num; a++){
+        cin >> arrNum;
+        arr.push_back(arrNum);
+        push_heap(arr.begin(),arr.end());
+    }
+    //make_heap(arr.begin(),arr.end());
+    sort_heap(arr.begin(),arr.end());
+    //sort(arr.begin(),arr.begin()+(Num/2));
+    //sort(arr.begin()+(Num/2),arr.end());
+    //inplace_merge(arr.begin(),arr.begin()+(Num/2),arr.end());
+ 
+    for(short b = 0; b < arr.size(); b++){
+        cout << arr[b] << '\n'; //endl은 flush을 사용하기 때문에 느리다...
+    }
+    return 0;
+}
