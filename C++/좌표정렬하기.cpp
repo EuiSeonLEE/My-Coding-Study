14f2b2a7986af7f719afeb6a87a36608552eb27f
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-04-02 12:16:59 +0900

    정렬(백준)

---
+++ C++/좌표정렬하기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair <int,int> XY1, pair <int,int> XY2){
+    if(XY1.first == XY2.first){
+        return XY1.second < XY2.second;
+    }
+    return XY1.first < XY2.first;
+}
+int main(void){
+    vector<pair<int,int>> XY;
+    int Num = 0;
+    int x = 0, y = 0;
+    cin >> Num;
+    
+    for(int a = 0; a < Num; a++){
+        cin >> x >> y;
+        XY.push_back(make_pair(x,y));
+    }
+    sort(XY.begin(),XY.end(), compare);
+    
+    for(auto a:XY){
+        printf("%d %d\n",a.first,a.second); //cout은 느리다...
+    }
+    return 0;
+}
