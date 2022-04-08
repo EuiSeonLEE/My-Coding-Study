35d895c77ecb02008d11fd21a36f37cb11c97fab
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-04-08 19:20:37 +0900

    정렬(프로그래머스)

---
+++ C++/K번째수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer, buffer;
+    for(int a = 0; a < commands.size(); a++){
+        for(int b = commands[a][0] - 1; b < commands[a][1]; b++){
+            answer.push_back(array[b]);
+            sort(answer.begin(), answer.end());
+        }
+        buffer.push_back(answer[commands[a][2] - 1]);
+        answer.clear();
+        answer.shrink_to_fit();
+    }
+    
+    
+    return buffer;
+}
