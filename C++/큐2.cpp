610f51a8060fcafbdd53fc6acc21f266d8e10592
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-05-06 23:54:02 +0900

    큐(백준)

---
+++ C++/큐2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    return 0;
+}
