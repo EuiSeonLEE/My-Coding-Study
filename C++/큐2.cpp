f72c4cdcbc3e4a97ad6ee73aa435301d456b3a52
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-05-12 01:41:36 +0900

    큐(백준)

--- C++/큐2.cpp
+++ C++/큐2.cpp
@@ -12,7 +12,36 @@ int main(void){
     cin.tie(NULL);
     cout.tie(NULL);
     
-    queue <int> q2;
+    queue <int> q;
+    string order;
+    int cnt = 0;
+    int num = 0;
+    cin >> cnt;
+
+    for(int a = 0; a < cnt; a++){
+        cin >> order;
+        if(order == "push"){
+            cin >> num;
+            q.push(num);
+        }
+        else if(order == "front"){
+            if(q.empty()) cout << -1 << "\n";
+            else cout << q.front() << "\n";
+        } 
+        else if(order == "back"){
+            if(q.empty()) cout << -1 << "\n";
+            else cout << q.back() << "\n";
+        } 
+        else if(order == "size") cout << q.size() << "\n";
+        else if(order == "empty") cout << q.empty() << "\n";
+        else if(order == "pop"){
+            if(q.empty()) cout << -1 << "\n";
+            else{
+             cout << q.front() << "\n";
+             q.pop();
+            }
+        }
+    }
 
     return 0;
 }
