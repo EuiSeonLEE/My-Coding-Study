6fa397befb3b6cd01dd6b91f0c79e5a4835dd223
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-05-13 23:43:03 +0900

    큐(백준)

---
+++ C++/카드2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    queue <int> q;
+   
+    return 0;
+}
