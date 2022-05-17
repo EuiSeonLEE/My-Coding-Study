91b8edd9f49b43a4e25d44aafa984363ace2b7bf
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-05-18 04:13:36 +0900

    큐(백준)

--- C++/카드2.cpp
+++ C++/카드2.cpp
@@ -12,7 +12,20 @@ int main(void){
     cin.tie(NULL);
     cout.tie(NULL);
     
-    queue <int> q4;
+    queue <int> q;
+    int N = 0, cnt = 0;
+
+    cin >> N;
+    for(int a = 1; a <= N; a++) q.push(a);
+    while(q.size() > 1){
+        cnt++;
+        if(cnt%2 == 1) q.pop();
+        else {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    cout << q.front() <<"\n";
 
     return 0;
 }
