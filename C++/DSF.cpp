ae8797d2d9c4c50d358d4274bfe7a58402bedb96
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-07-14 18:43:10 +0900

    덱큐(백준)

---
+++ C++/DSF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <numeric>
+
+using namespace std;
+//DSF함수 만들어보기
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    deque <int> dq;
+   
+
+    return 0;
+}
