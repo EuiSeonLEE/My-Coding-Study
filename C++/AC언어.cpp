0cbed3a9014f206f602a44406f404494995fd624
EuiSeonLEE <jncdfgh12@mju.ac.kr>
2022-07-20 22:00:19 +0900

    덱큐(백준)

--- C++/AC언어.cpp
+++ C++/AC언어.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 //1. isdigit함수는 문자열의 숫자를 판단하는 함수이다.(중요)
-//2. split은 왜 안돼는걸까??
+//2. split은 왜 안돼는걸까???
 /*deque<int> split(string &input, string delimiter){
 	deque<int> ret;
 	long long pos = 0;
